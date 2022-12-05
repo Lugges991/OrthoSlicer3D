b6235dc5d56b95a0fac065b7155a30f334a54a46
Lugges991 <29121982+Lugges991@users.noreply.github.com>
2022-12-05 10:58:13 +0100

    changed orthoslicer 3d to be able to set colormaps

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="OrthoSlicer3D",
+    version="0.0.2",
+    description="An orthographic slicer for 3D images",
+    url="https://github.com/Lugges991/OrthoSlicer3D",
+    author="Lugges991",
+    packages=["OrthoSlicer3D"],
+    install_requires=[],
+    classifiers=[],
+)
